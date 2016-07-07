322c9f9923e81ed55b380b93dfdd7876851b5c8e
michelleheh <xiaoyu.he22@gmail.com>
2016-07-07 11:54:21 -0700

    Complet reverseStr

--- 01_Array_and_Strings/02_reverseStr.js
+++ 01_Array_and_Strings/02_reverseStr.js
@@ -3,6 +3,8 @@ Reverse string
 */
 
 function reverseStr(str) {
+  if (str.length < 2) return str;
+  return str[str.length - 1] + reverseStr(str.slice(1, -1)) + str[0];
 };
 
 module.exports = reverseStr;
