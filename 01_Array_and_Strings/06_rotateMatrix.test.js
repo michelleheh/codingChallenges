a7a0bd23d80a921567e89c9082c4c9502745f20f
michelleheh <xiaoyu.he22@gmail.com>
2016-07-07 15:52:03 -0700

    Fix typo

--- 01_Array_and_Strings/06_rotateMatrix.test.js
+++ 01_Array_and_Strings/06_rotateMatrix.test.js
@@ -44,10 +44,10 @@ describe('Array_and_Strings', function() {
       ];
 
       let rotatedMatrix2 = [
-        [13,9,5,1],
-        [14,10,6,2],
-        [15,11,7,3],
-        [16,12,8,4]
+        [4,8,12,16],
+        [3,7,11,15],
+        [2,6,10,14],
+        [1,5,9,13]
       ];
 
       rotateMatrixInPlace(matrix);
