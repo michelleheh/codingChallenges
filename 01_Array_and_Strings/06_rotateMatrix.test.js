ce2951b5bb9273030986b21ea023fe1119030efe
michelleheh <xiaoyu.he22@gmail.com>
2016-07-07 15:49:57 -0700

    Add test

--- 01_Array_and_Strings/06_rotateMatrix.test.js
+++ 01_Array_and_Strings/06_rotateMatrix.test.js
@@ -36,8 +36,24 @@ describe('Array_and_Strings', function() {
         [1,4,7]
       ];
 
+      let matrix2 = [
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12],
+        [13,14,15,16]
+      ];
+
+      let rotatedMatrix2 = [
+        [13,9,5,1],
+        [14,10,6,2],
+        [15,11,7,3],
+        [16,12,8,4]
+      ];
+
       rotateMatrixInPlace(matrix);
+      rotateMatrixInPlace(matrix2);
       assert.deepEqual(matrix, rotatedMatrix);
+      assert.deepEqual(matrix2, rotatedMatrix2);
     });
 
   });
