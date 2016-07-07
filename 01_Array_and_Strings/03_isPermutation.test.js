d54c61fb95dc1275bd8b89f7f785c310b7d4e983
michelleheh <xiaoyu.he22@gmail.com>
2016-07-07 12:17:25 -0700

    Add test

--- 01_Array_and_Strings/03_isPermutation.test.js
+++ 01_Array_and_Strings/03_isPermutation.test.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const assert = require('chai').assert;
-const reverseStr = require('./03_isPermutation');
+const isPermutation = require('./03_isPermutation');
 
 describe('Array_and_Strings', function() {
   describe('isPermutation', function() {
     it('should decide if one string is permutation of anther', function() {
-      assert.equal(isPermutation('abc', 'bca'), true);
-      assert.equal(isPermutation('abc', 'aaa'), false);
       assert.equal(isPermutation('abc', 'a'), false);
+      assert.equal(isPermutation('abbc', 'bbca'), true);
+      assert.equal(isPermutation('abc', 'aaa'), false);
     });
+
   });
 });
