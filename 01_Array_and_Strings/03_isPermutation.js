36d9b3f40168c16f42f6eff3051d5fdd9a8ce04f
michelleheh <xiaoyu.he22@gmail.com>
2016-07-07 12:16:09 -0700

    Attemp isPermutation

--- 01_Array_and_Strings/03_isPermutation.js
+++ 01_Array_and_Strings/03_isPermutation.js
@@ -3,6 +3,21 @@ Given two strings, write a method to decide if one is a permutation of the other
 */
 
 function isPermutation(str1, str2) {
-}
+  if ( str1.length !== str2.length ) return false;
+  let str1hash = {};
+
+  for ( let i = 0; i < str1.length; i++ ) {
+    str1hash[str1[i]] = str1hash[str1[i]] + 1 || 1;
+  }
+
+  for ( let i = 0; i < str2.length; i++ ) {
+    if ( !str1hash[str2[i]] ) {
+      return false;
+    } else {
+      str1hash[str2[i]]--;
+    }
+  }
+  return true;
+};
 
 module.exports = isPermutation;
