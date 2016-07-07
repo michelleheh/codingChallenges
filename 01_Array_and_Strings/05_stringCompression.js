85e3be1e1b178c5de400022eb2ecad25d3ee7d2c
michelleheh <xiaoyu.he22@gmail.com>
2016-07-07 12:54:31 -0700

    Complete stringCompression

--- 01_Array_and_Strings/05_stringCompression.js
+++ 01_Array_and_Strings/05_stringCompression.js
@@ -6,6 +6,20 @@ Perform basic string compression
 */
 
 function stringCompression(str) {
+  let result = '';
+  let previous = str[0];
+  let count = 1;
+
+  for ( let i = 1; i <= str.length; i++ ) {
+    if ( str[i] !== previous ) {
+      result = result + previous + count;
+      previous = str[i];
+      count = 1;
+    } else {
+      count++;
+    }
+  }
+  return ( result.length < str.length ) ? result : str;
 }
 
 module.exports = stringCompression;
