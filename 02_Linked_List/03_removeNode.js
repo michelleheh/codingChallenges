8e4fc6c6417e4438136c8aedaaece4c6177601a7
michelleheh <xiaoyu.he22@gmail.com>
2016-07-08 11:55:19 -0700

    Add removeNode

---
+++ 02_Linked_List/03_removeNode.js
@@ -0,0 +1,11 @@
+/*
+Implement an algorithm to delete a node in the middle of a singly linked list fiven only access to that node
+Input: the node c from the linked list a -> b -> c -> d -> e
+Result: nothing is returned but the new linked list looks like a -> b -> d -> e
+*/
+
+function removeNode(linkedList, node) {
+
+};
+
+module.exports = removeNode;
