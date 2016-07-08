075247192821122db4b1af1a058d192d5e61b7c2
michelleheh <xiaoyu.he22@gmail.com>
2016-07-08 00:23:07 -0700

    Attemp findlastkth

--- 02_Linked_List/02_findLastkth.js
+++ 02_Linked_List/02_findLastkth.js
@@ -3,7 +3,13 @@ Implement an algorith to find the kth of last element of a singly linked List
 */
 
 function findkth(linkedList, k) {
-
+  let current = linkedList.head;
+  let arr = [];
+  while ( current.next ) {
+    arr.push(current);
+    current = current.next;
+  }
+  return arr[-k];
 };
 
 module.exports = findkth;
