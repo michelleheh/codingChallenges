2ec1734da7f11d67eb10acbcd010e1e7f2715fed
michelleheh <xiaoyu.he22@gmail.com>
2016-07-08 14:51:01 -0700

    Add decode

---
+++ 01_Array_and_Strings/09_decode.js
@@ -0,0 +1,16 @@
+/*
+write a function that decodes a string in the following way
+
+input: '0h2abe1iy'
+output: 'hey'
+
+what if there are more than single digit numbers?
+input: '10eeeeeeeeeeh2abe1iy' 
+output: 'hey'
+*/
+
+function decode() {
+  
+};
+
+module.exports = decode;
