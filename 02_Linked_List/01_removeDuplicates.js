277abd2448760f9ed948ad1528f23b5829d8777b
michelleheh <xiaoyu.he22@gmail.com>
2016-07-07 23:09:24 -0700

    Revise requirements

--- 02_Linked_List/01_removeDuplicates.js
+++ 02_Linked_List/01_removeDuplicates.js
@@ -1,7 +1,7 @@
 /*
 Write code to remove duplicates from an unsorted linked list
 FOLLOW UP
-How would you solve this problem if a temporary buffer is not allowed?
+How would you solve this problem if no hash table is not allowed?
 */
 
 function removeDuplicates(linkedList){
