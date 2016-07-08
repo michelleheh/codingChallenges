7f4cc0bb1d70b9d684d6d8dbbaa6003da99d20cb
michelleheh <xiaoyu.he22@gmail.com>
2016-07-08 12:36:56 -0700

    Revise test

--- 02_Linked_List/03_removeNode.js
+++ 02_Linked_List/03_removeNode.js
@@ -1,5 +1,5 @@
 /*
-Implement an algorithm to delete a node in the middle of a singly linked list fiven only access to that node
+Implement an algorithm to delete a node in the middle of a singly linked list given only access to that node
 Input: the node c from the linked list a -> b -> c -> d -> e
 Result: nothing is returned but the new linked list looks like a -> b -> d -> e
 */
