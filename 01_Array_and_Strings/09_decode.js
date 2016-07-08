872b7873bc53a44ee0d387e8078660870ed249f8
michelleheh <xiaoyu.he22@gmail.com>
2016-07-08 15:14:54 -0700

    Complete decode

--- 01_Array_and_Strings/09_decode.js
+++ 01_Array_and_Strings/09_decode.js
@@ -1,5 +1,5 @@
 /*
-write a function that decodes a string in the following way
+write a function that decodes a str in the following way
 
 input: '0h2abe1iy'
 output: 'hey'
@@ -9,8 +9,20 @@ input: '10eeeeeeeeeeh2abe1iy'
 output: 'hey'
 */
 
-function decode() {
-  
+function decode(str) {
+  let result = '';
+  let i = 0;
+  let count = str[0];
+  while ( i < str.length ) {
+    while ( str[i + 1].charCodeAt(0) >= 48 && str[i + 1].charCodeAt(0) <= 58 ) {
+      count += str[i + 1];
+      i++;
+    }
+    result += str[i + parseInt(count) + 1];
+    i += parseInt(count) + 2;
+    count = str[i]
+  }
+  return result;
 };
 
 module.exports = decode;
