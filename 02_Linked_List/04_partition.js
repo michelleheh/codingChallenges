5e412a071187fc04b82cb76698eebe0f6d8a84d4
michelleheh <xiaoyu.he22@gmail.com>
2016-07-08 14:42:07 -0700

    Complete partition

--- 02_Linked_List/04_partition.js
+++ 02_Linked_List/04_partition.js
@@ -3,7 +3,17 @@ write code to partition a linked list around a value x, such that all nodes less
 */
 
 function partition(linkedList, x) {
-
+  let current = linkedList.head;
+  while ( current.next ) {
+    if ( current.next.value <= x ) {
+      let temp = current.next
+      current.next = current.next.next;
+      temp.next = linkedList.head;
+      linkedList.head = temp;
+    } else {
+      current = current.next;
+    }
+  }
 };
 
 module.exports = partition;
