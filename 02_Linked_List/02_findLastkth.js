854f92cdeabea53989b30fef5dea66945e7b2d92
michelleheh <xiaoyu.he22@gmail.com>
2016-07-08 00:20:03 -0700

    Add findLastkth

---
+++ 02_Linked_List/02_findLastkth.js
@@ -0,0 +1,9 @@
+/*
+Implement an algorith to find the kth of last element of a singly linked List
+*/
+
+function findkth(linkedList, k) {
+
+};
+
+module.exports = findkth;
---
+++ 02_Linked_List/02_findLastkth.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const assert = require('chai').assert;
+const findLastkth = require('./02_findLastkth');
+
+describe('Array_and_Strings', function() {
+  describe('findLastkth', function() {
+    it('should return kth of last element', function() {
+      const node01 = {value: 1, next: null};
+      const node02 = {value: 2, next: null};
+      const node03 = {value: 3, next: null};
+      const node04 = {value: 4, next: null};
+      const node05 = {value: 5, next: null};
+      
+      let myList.head = node01;
+      node01.next = node02;
+      node02.next = node03;
+      node03.next = node04;
+      node04.next = node05;
+      
+      assert.equal(findLastkth(1).value, 5);
+      assert.equal(findLastkth(2).value, 4);
+
+    });
+  });
+});
