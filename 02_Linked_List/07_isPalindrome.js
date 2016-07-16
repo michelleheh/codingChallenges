199f5d3c2cfc93b9cb193e9b26dcbb951d151d8c
michelleheh <xiaoyu.he22@gmail.com>
2016-07-15 18:04:28 -0700

    Complete isPalindrome

--- 02_Linked_List/07_isPalindrome.js
+++ 02_Linked_List/07_isPalindrome.js
@@ -3,7 +3,15 @@ Implement a function to check if a linked list is a palindrome
 */
 
 function isPalindrome(linkedListHead) {
-
+  // base case 
+  if ( !linkedListHead || !linkedListHead.next ) return true;
+  let currentNode = linkedListHead;
+  while (currentNode.next.next) {
+    currentNode = currentNode.next;
+  }
+  if ( currentNode.next.value !== linkedListHead.value ) return false;
+  currentNode.next = null;
+  return isPalindrome(linkedListHead.next);
 };
 
 module.exports = isPalindrome;
