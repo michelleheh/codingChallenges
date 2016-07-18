bb8b99fa9437d7daeec97a14c09253cbb4aaaa83
Kevin Nguyen <account@kevinwin.com>
2016-07-18 12:20:32 -0700

    Add boolean evaluation

---
+++ 08_recursion_and_dynamic_programming/14_booleanEvaluation.js
@@ -0,0 +1,18 @@
+/*
+  Given a boolean expression consisting of the symbols 0 (false), 1(true),
+  & (AND), | (OR), and ^(XOR), and a desired boolean result value 'result',
+  implement a function to count the number of ways of parenthesizing the expression such
+  that it evaluates to 'result'
+
+  E.g.
+  countEval('1^0|0|1', false) -> 2
+  countEval('0&0&0&1^1|0', true) -> 10
+*/
+
+'use strict';
+
+function countEval() {
+  // Your code here
+}
+
+module.exports = countEval;
