fb295e3e89ff4cd4cac492fd137dd935af17a97f
michelleheh <xiaoyu.he22@gmail.com>
2016-07-19 20:46:10 -0700

    Update asyncMap test

--- 00_Toy_Problems/02_asyncMap.test.js
+++ 00_Toy_Problems/02_asyncMap.test.js
@@ -1,14 +1,22 @@
 'use strict';
 
 const assert = require('chai').assert;
-const asyncMap = require('./02_asyncMaps');
+const asyncMap = require('./02_asyncMap');
 
 describe('Array_and_Strings', function() {
   describe('asyncMap', function() {
     it('should square all numbers in the array', function() {
+
+      const square = function (num, doneCallback) {
+        setTimeout(function(){
+          return doneCallback(null, num * num);
+        }, 500);
+      };
+
       asyncMap([1, 2, 3, 4], square, (err, result) => {
         assert.equal(result, [1, 4, 9, 16]);
       });
+
     });
   });
 });
